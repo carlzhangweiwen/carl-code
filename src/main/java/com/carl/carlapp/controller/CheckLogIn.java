df4d2d01c7412fe719aac239b089a27baeebebc4
CarlZhang <CarlZhang@2015-1107-1310>
2015-12-02 23:46:10 +0800

    delete some file

---
+++ src/main/java/com/carl/carlapp/controller/CheckLogIn.java
@@ -0,0 +1,13 @@
+package com.carl.carlapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CheckLogIn {
+	
+	public String login(){
+		
+		
+		return "";
+	}
+}
