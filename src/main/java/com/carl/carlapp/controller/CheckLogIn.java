0f31485fb3c7ed29fcd6af27de5fc4e9a6b9108c
CarlZhang <CarlZhang@2015-1107-1310>
2015-12-03 00:21:31 +0800

    login

--- src/main/java/com/carl/carlapp/controller/CheckLogIn.java
+++ src/main/java/com/carl/carlapp/controller/CheckLogIn.java
@@ -1,13 +1,23 @@
 package com.carl.carlapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.carl.carlapp.model.UserInfo;
 
 @Controller
 public class CheckLogIn {
 	
-	public String login(){
+	@ResponseBody
+	@RequestMapping(value = "/tologin", method = RequestMethod.POST)
+	public String login(UserInfo user ){
+		if("carl".equals(user.getUsername()) && "123".equals(user.getPassword())){
+			
+			return "{\"errorCode\":\"0\",\"errorMsg\":\"ok\", \"token\":\"ss\"}";
+		}
 		
-		
-		return "";
+		return "{\"errorCode\":\"1001\",\"errorMsg\":\"password is wrong!\"}";
 	}
 }
---
+++ src/main/java/com/carl/carlapp/model/UserInfo.java
@@ -0,0 +1,20 @@
+package com.carl.carlapp.model;
+
+public class UserInfo {
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
